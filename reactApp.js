4c01b03efa4141271a0f1c80a75b663d73f86bc9
Nita <cburtoncareers@yahoo.com>
2020-08-14 16:59:07 -0400

    Added images

--- reactApp.js
+++ reactApp.js
@@ -1,11 +1,50 @@
 class Team extends React.Component {
   constructor(props) {
     super(props)
+
+
+    this.state = {
+      shots: 0,
+      score: 0
+    }
+
+
   }
-    render() {
+
+  shotHandler = () => {
+    let score = this.state.score
+
+    if (Math.random() > 0.5) {
+        score += 1
+    }
+
+    this.setState((state, props) => ({
+      shots: state.shots + 1,
+      score 
+    }))
+  }
+    
+  
+  
+  render() {
         return (
             <div className="Team">
-                Team
+              <h2>{this.props.name}</h2>
+
+
+              <div className="identity">
+                <img src={this.props.logo} alt={this.props.name}/>
+              </div>
+
+              <div>
+                <strong>Shots:</strong> {this.state.shots}
+              </div>
+
+              <div>
+                <strong>Score:</strong> {this.state.score}
+              </div>
+
+              <button onClick={this.shotHandler}>Shoot!</button>
             </div>
         )
     }
@@ -15,10 +54,21 @@ class Team extends React.Component {
 function App(props) {
     return (
       <div className="App">
+        <div className="stats">
         <Team 
         name="Russiaville Raccoons" 
-        logo="./assets/images/raccoons_disgusting.png"/>
+        logo="./assets/images/raccoons_disgusting.png"
+        />
+
+      <div className="versus">
+        <h2>VS</h2>
+        </div>  
+        
+        <Team
+        name="Sheridan Squirrels"
+        logo="./assets/images/squirrel_logo.jpg"/>
       </div>
+        </div>
     )
   }
   
