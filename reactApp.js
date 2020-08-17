dd7c84a67622ab3fa1783d9760141d171bcc4267
Nita <cburtoncareers@yahoo.com>
2020-08-17 15:58:16 -0400

    Finished normal and medium mode

--- reactApp.js
+++ reactApp.js
@@ -8,14 +8,23 @@ class Team extends React.Component {
       score: 0
     }
 
+    this.shotSound = new Audio('./assets/audio/attack.mp3')
+    this.scoreSound = new Audio('./assets/audio/cheers.mp3')
+
 
   }
 
   shotHandler = () => {
     let score = this.state.score
+    this.shotSound.play()
 
     if (Math.random() > 0.5) {
         score += 1
+
+        setTimeout(() => {
+        this.scoreSound.play()
+        }, 100)
+        
     }
 
     this.setState((state, props) => ({
@@ -27,6 +36,19 @@ class Team extends React.Component {
   
   
   render() {
+
+    let shotPercentageDiv
+
+    if(this.state.shots) {
+      const shotPercentage = Math.round((this.state.score / this.state.shots * 100))
+      shotPercentageDiv = (
+        <div>
+            <strong>Shooting %: {shotPercentage}</strong> 
+        </div>
+      )
+    }
+
+    
         return (
             <div className="Team">
               <h2>{this.props.name}</h2>
@@ -44,30 +66,72 @@ class Team extends React.Component {
                 <strong>Score:</strong> {this.state.score}
               </div>
 
+              {shotPercentageDiv}
+
               <button onClick={this.shotHandler}>Shoot!</button>
             </div>
         )
     }
 }
 
-
-function App(props) {
-    return (
-      <div className="App">
-        <div className="stats">
+function Game(props) {
+  return (
+    <div className="Game">
+      <h1>Welcome to {props.venue}</h1>
+      <div className="stats">
         <Team 
-        name="Russiaville Raccoons" 
-        logo="./assets/images/raccoons_disgusting.png"
+        name={props.visitingTeam.name}
+        logo={props.visitingTeam.logoSrc}
         />
 
       <div className="versus">
         <h2>VS</h2>
         </div>  
-        
+
         <Team
-        name="Sheridan Squirrels"
-        logo="./assets/images/squirrel_logo.jpg"/>
+        name={props.homeTeam.name}
+        logo={props.homeTeam.logoSrc}
+        />
       </div>
+    </div>
+    
+  )
+  
+}
+
+
+function App(props) {
+  const raccoons= {
+    name: "Russiaville Raccoons",
+    logoSrc:"./assets/images/raccoons_disgusting.png"
+  }
+
+  const squirrels= {
+    name: "Sheridan Squirrels",
+    logoSrc: "./assets/images/squirrel_logo.jpg"
+  }
+
+  const bunnies = {
+    name: "Burlington Bunnies",
+    logoSrc: "./assets/images/bunny_logo.jpg"
+  }
+
+  const hounds ={
+    name: "Hammond Hounds",
+    logoSrc: "./assets/images/hound_logo.jpg"
+  }
+    return (
+      <div className="App">
+        <Game 
+        venue="Union 525 Gem"
+        homeTeam={squirrels}
+        visitingTeam={raccoons} 
+        />
+        <Game  
+        venue="Sheridan Arena" 
+        homeTeam={bunnies}
+        visitingTeam={hounds}
+        />
         </div>
     )
   }
